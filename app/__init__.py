6fde86722745d263768f2976bd59fb72bd9eb916
jack <jackthebestripper@yahoo.com>
2018-10-03 05:26:07 +0000

    Sentry integrated

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from config import Config
+
+sentry_sdk.init(
+    dsn="https://45e872aea79e46f6a7271dcecef589eb@sentry.io/1293333",
+    integrations=[FlaskIntegration()]
+)
 
 app = Flask(__name__)
 app.config.from_object(Config)
